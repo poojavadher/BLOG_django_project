fc6eeaf74cb859de106c6bb2dfb324c0ad6479b0
Pooja Vadher <poojavadher24@gmail.com>
2022-03-29 10:48:48 +0530

    Updated ststic root

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = Path(BASE_DIR,'media')
 MEDIA_URL = '/media/'
